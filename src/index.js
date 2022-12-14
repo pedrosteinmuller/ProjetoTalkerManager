5afce67c52b510d2b256f3ecfbca627470632364
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2022-12-14 15:56:56 -0300

    fix: error lint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const path = require("path");
-const fs = require("./utils/fsdata");
+const express = require('express');
+const path = require('path');
+const fs = require('./utils/fsdata');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND_STATUS = 404;
-const PORT = "3000";
+const PORT = '3000';
 
-const TALKERS_FILE_DATA = path.resolve(__dirname, "./talker.json");
+const TALKERS_FILE_DATA = path.resolve(__dirname, './talker.json');
 
-app.get("/talker", async (_request, response) => {
+app.get('/talker', async (_request, response) => {
   const talkers = await fs.readFile(TALKERS_FILE_DATA);
   if (talkers.length === 0) {
     return response.status(HTTP_OK_STATUS).send([]);
@@ -19,23 +19,23 @@ app.get("/talker", async (_request, response) => {
   return response.status(HTTP_OK_STATUS).send(talkers);
 });
 
-app.get("/talker/:id", async (request, response) => {
+app.get('/talker/:id', async (request, response) => {
   const { id } = request.params;
   const talkers = await fs.readFile(TALKERS_FILE_DATA);
   const talkerId = talkers.find((person) => person.id === Number(id));
   if (!talkerId) {
     return response
       .status(HTTP_NOT_FOUND_STATUS)
-      .send({ message: "Pessoa palestrante não encontrada" });
+      .send({ message: 'Pessoa palestrante não encontrada' });
   }
   return response.status(HTTP_OK_STATUS).send(talkerId);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log('Online');
 });
