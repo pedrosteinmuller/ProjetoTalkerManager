5e5be211fa7af0ad981caef8c4e0ca3dd7c410a2
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2022-12-14 15:18:48 -0300

    feat: iniciando requisito 1 criando o endpoint get /talker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
-// first commit
+const fs = require('fs/promises');
+
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKERS_FILE_DATA = './talker.json';
+
+app.get('/talker', async (_request, response) => {
+  const talkers = JSON.parse(await fs.readFile(TALKERS_FILE_DATA));
+  if(talkers.length === 0){
+    return response.status(HTTP_OK_STATUS).send([]);
+  }
+  return response.status(HTTP_OK_STATUS).send(talkers);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
